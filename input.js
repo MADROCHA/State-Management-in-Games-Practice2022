aa5e81c867e00b1d31da2a5d6f648f0647b38f81
MADROCHA <madlarocha@gmail.com>
2022-08-22 16:45:54 +0200

    this.lastKey_modify_awsd//_stateManageGames_

--- input.js
+++ input.js
@@ -2,13 +2,19 @@ export default class InputHandler {
     constructor(){
         this.lastKey = '';
         window.addEventListener('keydown', (e) => {           
-             /* console.log(e.key) */
+            console.log(e.key)
             switch(e.key){
                 case "ArrowLeft":
                     this.lastKey = "PRESS left";
                     break 
                 case "ArrowRight":
                     this.lastKey = "PRESS right";
+                    break 
+                case "s":
+                    this.lastKey = "PRESS s";
+                    break 
+                case "w":
+                    this.lastKey = "PRESS w";
                     break 
             }
         });
@@ -19,7 +25,13 @@ export default class InputHandler {
                     break 
                 case "ArrowRight":
                     this.lastKey = "RELEASE right";
-                    break 
+                    break
+                case "s":
+                    this.lastKey = "RELEASE s";
+                    break  
+                case "w":
+                    this.lastKey = "RELEASE w";
+                    break  
             }
         });
     }
--- state.js
+++ state.js
@@ -1,6 +1,8 @@
 export const states = {
     STANDING_LEFT: 0,
     STANDING_RIGHT: 1,
+    SITTING_LEFT: 2,
+    SITTING_RIGHT: 3,
 }
 
 class State {
@@ -20,6 +22,7 @@ export class StandingLeft extends State {
     }
     handleInput(input){
         if (input === 'PRESS right') this.player.setState(states.STANDING_RIGHT);
+        else if (input === 'PRESS s')this.player.setState(states.SITTING_LEFT);
         
     }
 }
@@ -35,5 +38,35 @@ export class StandingRight extends State {
     }
     handleInput(input){
         if (input === 'PRESS left') this.player.setState(states.STANDING_LEFT);
+        else if (input === 'PRESS s')this.player.setState(states.SITTING_RIGHT);
+    }
+}
+
+export class SittingLeft extends State {
+    constructor(player){
+            super('SITTING LEFT');
+            this.player = player;
+    }
+    enter(){
+        this.player.frameY = 9;
+
+    }
+    handleInput(input){
+        if (input === 'PRESS right') this.player.setState(states.SITTING_RIGHT);
+        else if(input === 'PRESS w') this.player.setState(states.STANDING_LEFT);
+    }
+}
+export class SittingRight extends State {
+    constructor(player){
+            super('SITTING RIGHT');
+            this.player = player;
+    }
+    enter(){
+        this.player.frameY = 8;
+
+    }
+    handleInput(input){
+        if (input === 'PRESS left') this.player.setState(states.SITTING_LEFT);
+        else if(input === 'PRESS w') this.player.setState(states.STANDING_RIGHT);
     }
 }
