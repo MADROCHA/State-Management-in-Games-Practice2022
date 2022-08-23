7aec899665b2161cfe583a70a084f32dd72a7820
MADROCHA <madlarocha@gmail.com>
2022-08-23 02:52:09 +0200

    _//_styles.css_loading_&_body.->backgroundColor_&_textColor//_utils.js_basicControlsCanvas//_GameStates_0.1_

--- utils.js
+++ utils.js
@@ -1,5 +1,13 @@
 export function drawStatusText(context, input, player){
     context.font = '30px Helvetica';
+    context.fillStyle = 'Azure';
     context.fillText('Last input: ' + input.lastKey, 40, 80);
     context.fillText('Active State: ' + player.currentState.state, 40, 140);
+    
+    /* 1.0 */
+    context.fillText('W to Jump ', 40, 300);
+    context.fillText('S to Crouch ', 40, 360);
+    context.fillText('A to move Left  ', 40, 420);
+    context.fillText('D to move Right  ', 40, 480);
+    /* 1.0 */
 }
